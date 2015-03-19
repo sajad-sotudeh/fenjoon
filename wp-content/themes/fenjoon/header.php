470377be80db72b5af76e070d8417a634fe0ac55
peyman-mohamadpour <peyman.mohamadpour@yahoo.com>
2015-03-19 15:54:04 +0330

    Added basic code to index, header, footer
    Blogging basic functionality added to index.php, header.php and
    footer.php

---
+++ wp-content/themes/fenjoon/header.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+<body>
+	<div id="wrapper">
+		<div id="header"><h1>HEADER</h1></div>
