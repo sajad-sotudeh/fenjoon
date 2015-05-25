520bfbd0f1e497eb88b621105a979bd8f1e62c83
amin-gholami <amin.gholami94@gmail.com>
2015-05-25 17:22:02 +0430

    Homepage is Added!
    homepage template is added to theme.
    + some addition in fenjoon plugin

---
+++ wp-content/plugins/fenjoon/fenjoon-init.php
@@ -0,0 +1,2 @@
+<?php
+ load_plugin_textdomain('fenjoon', false, basename(dirname(__FILE__)).'/lang');
--- wp-content/themes/fenjoon/header.php
+++ wp-content/themes/fenjoon/header.php
@@ -1,12 +1,38 @@
 <html>
 <head>
 	<title><?php bloginfo('name'); ?></title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">		
 </head>
 <body>
 	<div id="wrapper">
-		<div id="header"><h1>HEADER</h1></div>
+		<div id="header"></div>
 		<?php 
 		$workforce_values = fjn_editors_by_free_time( );
 	//	var_dump($workforce_values);
 		?>
+		
+		<section>
+			
+				<img src="work1.gif" height="100" width="100">
+				<p> logo </p>
+			
+		</section>
+
+		<section>
+			<form action="action_page.php">
+				Search:
+				<input type="search" name="search">
+				<input type="submit">
+			</form>
+		
+		
+		</section>
+		
+		<h1> Title Of the Site </h1>
+	
+	<nav>
+		<a href="/m1/">M1</a> |
+		<a href="/m2/">M2</a> |
+		<a href="/m3/">M3</a> |
+		<a href="/m4/">M4</a>
+	</nav>
